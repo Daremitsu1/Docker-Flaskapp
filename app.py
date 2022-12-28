9dd93435e8052d842e764b89c086d55c80a0b227
Aviparna Biswas <54842807+Daremitsu1@users.noreply.github.com>
2022-12-28 10:38:29 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -1,21 +1,52 @@
+# 1. Import the dependencies
 from flask import Flask, request
 import pandas as pd
 import numpy as np
 import pickle
-
+import flasgger
+from flasgger import Swagger
 
 app = Flask(__name__)
+Swagger(app)
 
-# Read the pickle file
+# 2. Read the pickle file
 pickle_in = open('classifier.pkl', 'rb')
 classifier = pickle.load(pickle_in)
 
+# 3. Define the GET Method
 @app.route('/')
 def welcome():
     return "Welcome All!"
 
-@app.route('/predict')
+@app.route('/predict', methods=["GET"])
 def predict_note_authentication():
+
+    """Let's Authenticate the Banks Note 
+    This is using docstrings for specifications.
+    ---
+    parameters:  
+      - name: variance
+        in: query
+        type: number
+        required: true
+      - name: skewness
+        in: query
+        type: number
+        required: true
+      - name: curtosis
+        in: query
+        type: number
+        required: true
+      - name: entropy
+        in: query
+        type: number
+        required: true
+    responses:
+        200:
+            description: The output values
+        
+    """
+
     variance = request.args.get('variance')
     skewness = request.args.get('skewness')
     curtosis = request.args.get('curtosis')
@@ -23,11 +54,28 @@ def predict_note_authentication():
     prediction = classifier.predict([[variance, skewness, curtosis, entropy]])
     return "The predicted value is" + str(prediction)
 
+# 4. Define the POST Method
 @app.route('/predict_file', methods=["POST"])
 def predict_note_file():
+    """Let's Authenticate the Banks Note 
+    This is using docstrings for specifications.
+    ---
+    parameters:
+      - name: file
+        in: formData
+        type: file
+        required: true
+      
+    responses:
+        200:
+            description: The output values
+        
+    """
+
     df_test = pd.read_csv(request.files.get("file"))
     prediction = classifier.predict(df_test)
     return "The predicted values for the csv is" + str(list(prediction))
 
+# 5. Run the app
 if __name__ == "__main__":
     app.run()
